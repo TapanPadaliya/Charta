819715e95d25d1bbefed64aecc0036d876188603
Tapan <120307305+TapanPadaliya@users.noreply.github.com>
2023-11-14 22:55:33 +0530

    bug fix 4

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4 flex items-center justify-center flex-col space-y-3">
-    <img src="/images/charta-white.png" class="h-28 w-28" />
+    <img src="/images/Charta-white.png" class="h-28 w-28" />
     <p class="text-2xl font-bold">Welcome To Charta</p>
   </div>
 </template>
