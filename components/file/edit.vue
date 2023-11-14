51ac246fae692f800171269ae63fdc58d90854dd
TapanPadaliya <tapanpadaliya02@gmail.com>
2023-11-14 20:41:03 +0530

    changes

--- components/file/edit.vue
+++ components/file/edit.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
+  <div class="">
     <QuillEditor
       v-model:content="dataField"
       toolbar="full"
       contentType="html"
       class="border border-black"
-      @update:content="updateConent"
+      @update:content="saveContent"
     />
+  </div>
+  <div
+    class="absolute bottom-4 right-4 p-2 bg-zinc-700 rounded-md transition-all duration-300"
+  >
+    <IconsLoading class="text-white" v-if="isLoading" />
+    <IconsSaved class="text-green-500" v-else />
   </div>
 </template>
 
 <script setup>
 // Imports
+import { useDebounceFn } from "@vueuse/core";
 import { QuillEditor, Delta } from "@vueup/vue-quill";
 import { onMounted, defineProps, ref } from "vue";
 import { useFileStore } from "@/store/fileStore";
@@ -29,6 +36,7 @@ const { $bus } = useNuxtApp();
 const files = useFileStore();
 const watchedId = ref(null);
 const dataField = ref(null);
+const isLoading = ref(false);
 
 // Setup On Mounted
 onMounted(async () => {
@@ -36,7 +44,13 @@ onMounted(async () => {
 });
 
 // Update Content & Save
-const updateConent = async (data) => {
-  console.log("Updating Content", data);
+const saveContent = () => {
+  isLoading.value = true;
+  updateConent();
 };
+
+const updateConent = useDebounceFn(async () => {
+  console.log("Updating Contnet :", dataField.value);
+  isLoading.value = false;
+}, 4000);
 </script>
